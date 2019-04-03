1c6bd2b61ddf0fee2d0eeba28900fa1a32def02b
Karmanya Suryavanshi <suryavanshikarmanya@gmail.com>
2019-04-03 16:16:48 -0500

    completed

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,20 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    let last = someStr.length;
+    
+    str = someStr.toLowerCase();
+    let reversed = '';
+
+    for(let i = last-1; i >= 0; i-- ){
+            reversed = reversed + str[i];
+    }
+    if(reversed === str){
+            return 'Given String is a Palindrome';
+    }
+    else{
+            return 'Given String is not a Palindrome';
+    }
 }
+
+console.log(isPalindrome('snsnsA'));
