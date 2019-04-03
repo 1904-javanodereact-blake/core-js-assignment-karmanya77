4b4e9e836382fea6a899ee2644096b86b10bf8b1
Karmanya Suryavanshi <suryavanshikarmanya@gmail.com>
2019-04-02 19:27:43 -0500

    completed

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -3,4 +3,19 @@ Define function: fib(n)
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
 
+    let firstNumber = 0;
+    let secondNumber = 1;
+    let sum = 0;
+    console.log(firstNumber);
+    console.log(secondNumber);
+
+    for(let i = 0; i < n-1; i++) {
+        sum = firstNumber + secondNumber;
+        console.log(sum);
+        firstNumber = secondNumber;
+        secondNumber = sum;
+
+    }
 }
+
+fib(12);
