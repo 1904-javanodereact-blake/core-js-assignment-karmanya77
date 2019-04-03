bf34d1217f0e1d71c7de806b0b2c4df37709e7e2
Karmanya Suryavanshi <suryavanshikarmanya@gmail.com>
2019-04-03 09:18:43 -0500

    completed

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -5,4 +5,15 @@ If incorrect input is entered, throw an exception stating the input was incorrec
 For example substring('hello', 0, 3) should give 'hel' */
 function substring(someStr, startIndex, endIndex) {
 
+    let sub = '';
+    if(typeof(someStr) == "string"){
+        sub = someStr.slice(startIndex,endIndex);
+        return sub;
+    }
+    else{
+        console.log("The input was incorrect.");
+    }
 }
+
+let str = 'hello world';
+console.log(substring(str,2,5));
