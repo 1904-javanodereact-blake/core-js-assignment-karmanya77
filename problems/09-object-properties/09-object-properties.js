fe5cafc510555929d7cfea155df4197475e33b4a
Karmanya Suryavanshi <suryavanshikarmanya@gmail.com>
2019-04-07 20:04:24 -0600

    completed

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,14 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    console.log(someObj);
 }
+
+let spaceShip = {
+    name : 'Millenium Falcon',
+    maxSpeed : 'can do kessel run in 12 parcecs',
+    weight : 'heavy',
+    passengerLoad : 20
+};
+
+objectProperties(spaceShip);
